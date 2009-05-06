478d2aa32287060b66a5dec6d7db041676ad4d2f
ctaggart <cameron.taggart@gmail.com>
2009-05-05 22:07:19 -0700

    started work on the .proto file parser using FParsec xUnit tests for the parsers are working

--- ProtoParser.Test/ProtoParser.Test.fs
+++ ProtoParser.Test/ProtoParser.Test.fs
@@ -2,11 +2,81 @@
 namespace Froto.ProtoParser
 module Test
 
+open System
 open Xunit
+open FParsec.Primitives
+open FParsec.CharParser
 open Froto.ProtoParser
 
-[<Fact>]    
-let capitalize() =     
-  Assert.Equal(capitalize "a", "A")
-  Assert.NotEqual(capitalize "a", "a")
-  Assert.NotEqual(capitalize "b", "A")
+let equal expected actual = Assert.Equal(expected, actual)
+let notEqual expected actual = Assert.NotEqual(expected, actual)
+let isTrue condition = Assert.True(condition)
+let isFalse condition = Assert.False(condition)
+
+let throwParserFailure pr =
+  Assert.ThrowsDelegate(fun () ->
+    match pr with
+    | Success(_) -> () 
+    | Failure(errorMsg,_) -> failwith errorMsg
+  )
+
+let assertParseSuccess (pr:ParserResult<_>) = Assert.DoesNotThrow(throwParserFailure pr)
+let assertParseFailure (pr:ParserResult<_>) = Assert.Throws<FailureException>(throwParserFailure pr)
+
+/// test a parser on a string
+let parseString p s = runParserOnString p () String.Empty s
+let canParse p s = assertParseSuccess(parseString p s)
+let canNotParse p s = assertParseFailure(runParserOnString p () String.Empty s) |> ignore
+
+let isParseResult p s expected =
+  isTrue(
+    match parseString p s with
+    | Success actual -> actual = expected
+    | Failure (_,_) -> false
+  )
+
+/// testing xUnit setup :)
+[<Fact>]
+let capitalizeTest() =
+  equal (capitalize "a") "A"
+  equal (capitalize "a") "A"
+  notEqual (capitalize "a") "a"
+  notEqual (capitalize "b") "A"
+
+[<Fact>]
+let canParseTest() =
+  let pDuck = pstring "duck"
+  canParse pDuck "duck"
+  canNotParse pDuck "goose"
+
+[<Fact>]
+let isNewLineTest() =
+  isTrue (isNewLine '\r')
+  isTrue (isNewLine '\n')
+  isTrue (isNewLineFalse 'c')
+
+[<Fact>]
+let spacesTest() =
+  canParse spaces1 "   " // 3 spaces
+  canParse spaces1 "      " // 3 tabs
+  canNotParse spaces1 "f"
+  canNotParse spaces1 "fail"
+  canParse spaces "     spaces tab space tab before this"
+  
+[<Fact>]
+let pMessageNameTest() =
+  canParse pMessageName "message SearchRequest"
+  canNotParse pMessageName "message"
+  isParseResult pMessageName "message SearchRequest" "SearchRequest"
+  
+[<Fact>]
+let pWordTest() =
+  canParse pWord "word"
+  canNotParse pWord " "
+  isParseResult pWord "word " "word"
+  
+[<Fact>]
+let pScalarType() =
+  isParseResult pScalarType "double" ProtoDouble
+  isParseResult pScalarType "float" ProtoFloat
+  //canNotParse pScalarType "dbl"
--- ProtoParser/ProtoParser.fs
+++ ProtoParser/ProtoParser.fs
@@ -2,5 +2,92 @@
 namespace Froto
 module ProtoParser
 
-/// testing xunit :)
+// http://code.google.com/apis/protocolbuffers/docs/proto.html
+// http://www.quanttec.com/fparsec/
+
+open FParsec.Primitives
+open FParsec.CharParser
+
+/// testing xUnit setup :)
 let capitalize (s:string) = s.ToUpper()
+
+let whitespaceChars = [|' '; '\t'|]
+let isWhitespaceChar c = c = ' ' || c = '\t'
+let trim (s:string) = s.Trim whitespaceChars
+
+/// the character is not a newline character
+/// a carriage return character or a line feed character
+/// http://en.wikipedia.org/wiki/Newline
+let isNewLine c = c = '\r' || c = '\n'
+let isNewLineFalse c = isNewLine c = false
+
+// overriding spaces, newlines are important for comments
+// http://www.quanttec.com/fparsec/#spaces
+/// skip zero or more whitespace characters, never fails
+let spaces : State<unit> -> Reply<unit,State<unit>> =
+  skipManySatisfy isWhitespaceChar
+let spaces1 : State<unit> -> Reply<unit,State<unit>> =
+  skipMany1Satisfy isWhitespaceChar
+
+let pWord : State<unit> -> Reply<string,State<unit>> = 
+  many1Chars (asciiLetter <|> digit <|> anyOf "_")
+
+let pMessageName = pstring "message" >>. spaces1 >>. many1Chars (asciiLetter <|> digit)
+
+type ScalarType =
+  | ProtoDouble
+  | ProtoFloat
+  | ProtoInt32
+  | ProtoInt64
+  | ProtoUInt32
+  | ProtoUInt64
+  | ProtoSInt32
+  | ProtoSInt64
+  | ProtoFixed32
+  | ProtoFixed64
+  | ProtoSFixed32
+  | ProtoSFixed64
+  | ProtoBool
+  | ProtoString
+  | ProtoBytes
+  
+let scalarTypeMap =
+  [
+    "double", ProtoDouble;
+    "float", ProtoFloat;
+    "int32", ProtoInt32;
+    "int64", ProtoInt64;
+    "uint32", ProtoUInt32;
+    "uint64", ProtoUInt64;
+    "sint32", ProtoSInt32;
+    "sint64", ProtoSInt64;
+    "fixed32", ProtoFixed32;
+    "fixed64", ProtoFixed64;
+    "sfixed32", ProtoSFixed32;
+    "sfixed64", ProtoSFixed64;
+    "bool", ProtoBool;
+    "string", ProtoString;
+  ]
+  |> Map.of_list
+
+/// http://code.google.com/apis/protocolbuffers/docs/proto.html#scalar
+let toScalarType s = scalarTypeMap.[s]
+let isScalarType s = scalarTypeMap.ContainsKey s
+
+//let pScalarType =
+//  pWord |>> fun w ->
+//    if isScalarType w then
+//      toScalarType w
+//    else 
+//      failwithf "unknown scalar type: %s" w
+
+let pScalarType =
+  parse {
+    let! w = pWord
+//    if isScalarType w then
+//      return toScalarType(w)
+//    else
+//      //fail ("unknown scalar type: %s" w)
+    return toScalarType(w)
+  }
+  
