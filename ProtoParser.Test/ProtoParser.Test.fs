3b36557661b58c06274c9798dd1c3f5f42e39a81
Cameron Taggart <cameron.taggart@gmail.com>
2012-08-18 16:31:48 -0700

    switched to 4 space indentation to match other projects

--- ProtoParser.Test/ProtoParser.Test.fs
+++ ProtoParser.Test/ProtoParser.Test.fs
@@ -10,11 +10,11 @@ open FParsec.CharParsers
 open Froto.ProtoParser
 
 let throwParserFailure pr =
-  Assert.ThrowsDelegate(fun () ->
-    match pr with
-    | Success(_) -> () 
-    | Failure(errorMsg, _, _) -> failwith errorMsg
-  )
+    Assert.ThrowsDelegate(fun () ->
+        match pr with
+        | Success(_) -> () 
+        | Failure(errorMsg, _, _) -> failwith errorMsg
+    )
 
 let assertParseSuccess (pr:ParserResult<_,_>) = Assert.DoesNotThrow(throwParserFailure pr)
 let assertParseFailure (pr:ParserResult<_,_>) = Assert.Throws<Exception>(throwParserFailure pr)
@@ -28,82 +28,82 @@ let canNotParse p s = assertParseFailure(runParserOnString p () String.Empty s) 
 /// looking for froto\test\file when
 /// current assembly is froto\Project\bin\Configuration\Project.dll
 let getTestFile file =
-   let codeBase = Reflection.Assembly.GetExecutingAssembly().CodeBase
-   let assemblyPath = DirectoryInfo (Uri codeBase).LocalPath
-   let solutionPath = (assemblyPath.Parent.Parent.Parent.Parent).FullName
-   Path.Combine(solutionPath, Path.Combine("test",file))
+     let codeBase = Reflection.Assembly.GetExecutingAssembly().CodeBase
+     let assemblyPath = DirectoryInfo (Uri codeBase).LocalPath
+     let solutionPath = (assemblyPath.Parent.Parent.Parent.Parent).FullName
+     Path.Combine(solutionPath, Path.Combine("test",file))
 
 let parseFile p f = runParserOnFile p () (getTestFile f) Text.Encoding.UTF8 
 let canParseFile p f = assertParseSuccess(parseFile p f)
 let canNotParseFile p f = assertParseFailure(parseFile p f)
 
 let isParseResult p s expected =
-  match parseString p s with
-  | Success (actual, _, _) -> actual = expected
-  | Failure (_, _, _) -> false
-  |> should be True
+    match parseString p s with
+    | Success (actual, _, _) -> actual = expected
+    | Failure (_, _, _) -> false
+    |> should be True
 
 /// testing xUnit setup :)
 [<Fact>]
 let capitalizeTest() =
-  "A" |> should equal (capitalize "a") 
-  "a" |> should not' (equal (capitalize "a"))
-  "A" |> should not' (equal (capitalize "b"))
+    "A" |> should equal (capitalize "a") 
+    "a" |> should not' (equal (capitalize "a"))
+    "A" |> should not' (equal (capitalize "b"))
 
 [<Fact>]
 let canParseTest() =
-  let pDuck = pstring "duck"
-  canParse pDuck "duck"
-  canNotParse pDuck "goose"
+    let pDuck = pstring "duck"
+    canParse pDuck "duck"
+    canNotParse pDuck "goose"
 
 [<Fact>]
 let isNewLineTest() =
-  isNewLine '\r' |> should be True
-  isNewLine '\n'  |> should be True
-  isNewLineFalse 'c' |> should be True
+    isNewLine '\r' |> should be True
+    isNewLine '\n'    |> should be True
+    isNewLineFalse 'c' |> should be True
 
 [<Fact>]
 let spacesTest() =
-  canParse spaces1 "   " // 3 spaces
-  canParse spaces1 "      " // 3 tabs
-  canNotParse spaces1 "f"
-  canNotParse spaces1 "fail"
-  canParse spaces "     spaces tab space tab before this"
-  
+    canParse spaces1 "     " // 3 spaces
+    canParse spaces1 "            " // 3 tabs
+    canNotParse spaces1 "f"
+    canNotParse spaces1 "fail"
+    canParse spaces "         spaces tab space tab before this"
+    
 [<Fact>]
 let pMessageNameTest() =
-  canParse pMessageName "message SearchRequest"
-  canNotParse pMessageName "message"
-  isParseResult pMessageName "message SearchRequest" "SearchRequest"
-  
+    canParse pMessageName "message SearchRequest"
+    canNotParse pMessageName "message"
+    isParseResult pMessageName "message SearchRequest" "SearchRequest"
+    
 [<Fact>]
 let pWordTest() =
-  canParse pWord "word"
-  canNotParse pWord " "
-  isParseResult pWord "word " "word"
-  
+    canParse pWord "word"
+    canNotParse pWord " "
+    isParseResult pWord "word " "word"
+    
 [<Fact>]
 let pScalarTypeTest() =
-  isParseResult pScalarType "double" ProtoDouble
-  isParseResult pScalarType "float" ProtoFloat
-  canNotParse pScalarType "dbl"
+    isParseResult pScalarType "double" ProtoDouble
+    isParseResult pScalarType "float" ProtoFloat
+    canNotParse pScalarType "dbl"
 
 [<Fact>]
 let pFieldRuleTest() =
-  isParseResult pFieldRule "required" Required
-  isParseResult pFieldRule "optional" Optional
-  isParseResult pFieldRule "repeated" Repeated
-  canNotParse pFieldRule "rep"
+    isParseResult pFieldRule "required" Required
+    isParseResult pFieldRule "optional" Optional
+    isParseResult pFieldRule "repeated" Repeated
+    canNotParse pFieldRule "rep"
 
 [<Fact>]
 let pFieldTest() =
-  canParse pField "required string query = 1;"
-  canParse pField "required   string   query=34245   ;"
+    canParse pField "required string query = 1;"
+    canParse pField "required     string     query=34245     ;"
 
-[<Fact>]  
+[<Fact>]    
 let findTestFileTest() =
-  File.Exists (getTestFile "SearchRequest.proto") |> should be True
+    File.Exists (getTestFile "SearchRequest.proto") |> should be True
 
 [<Fact>]
 let pMessageTest() =
-  canParseFile pMessage "SearchRequest.proto"
+    canParseFile pMessage "SearchRequest.proto"
--- ProtoParser/ProtoParser.fs
+++ ProtoParser/ProtoParser.fs
@@ -32,110 +32,110 @@ let spaces1 = skipMany1Satisfy isWhitespaceChar
 // perhaps add comments?
 let isEndOfLineChar c = c = '\n' || c = '\r'
 let pEndOfLine =
-  skipManySatisfy isEndOfLineChar
+    skipManySatisfy isEndOfLineChar
 
 let pWord = many1Chars (asciiLetter <|> digit <|> anyOf "_")
 
 type ScalarType =
-  | ProtoDouble
-  | ProtoFloat
-  | ProtoInt32
-  | ProtoInt64
-  | ProtoUInt32
-  | ProtoUInt64
-  | ProtoSInt32
-  | ProtoSInt64
-  | ProtoFixed32
-  | ProtoFixed64
-  | ProtoSFixed32
-  | ProtoSFixed64
-  | ProtoBool
-  | ProtoString
-  | ProtoBytes
-  
+    | ProtoDouble
+    | ProtoFloat
+    | ProtoInt32
+    | ProtoInt64
+    | ProtoUInt32
+    | ProtoUInt64
+    | ProtoSInt32
+    | ProtoSInt64
+    | ProtoFixed32
+    | ProtoFixed64
+    | ProtoSFixed32
+    | ProtoSFixed64
+    | ProtoBool
+    | ProtoString
+    | ProtoBytes
+    
 let scalarTypeMap =
-  [
-    "double", ProtoDouble;
-    "float", ProtoFloat;
-    "int32", ProtoInt32;
-    "int64", ProtoInt64;
-    "uint32", ProtoUInt32;
-    "uint64", ProtoUInt64;
-    "sint32", ProtoSInt32;
-    "sint64", ProtoSInt64;
-    "fixed32", ProtoFixed32;
-    "fixed64", ProtoFixed64;
-    "sfixed32", ProtoSFixed32;
-    "sfixed64", ProtoSFixed64;
-    "bool", ProtoBool;
-    "string", ProtoString;
-  ]
-  |> Map.ofList
+    [
+        "double", ProtoDouble;
+        "float", ProtoFloat;
+        "int32", ProtoInt32;
+        "int64", ProtoInt64;
+        "uint32", ProtoUInt32;
+        "uint64", ProtoUInt64;
+        "sint32", ProtoSInt32;
+        "sint64", ProtoSInt64;
+        "fixed32", ProtoFixed32;
+        "fixed64", ProtoFixed64;
+        "sfixed32", ProtoSFixed32;
+        "sfixed64", ProtoSFixed64;
+        "bool", ProtoBool;
+        "string", ProtoString;
+    ]
+    |> Map.ofList
 
 /// http://code.google.com/apis/protocolbuffers/docs/proto.html#scalar
 let toScalarType s = scalarTypeMap.[s]
 let isScalarType s = scalarTypeMap.ContainsKey s
 
 let pScalarType =
-  pWord >>= fun w ->
-    if isScalarType w then
-      toScalarType w |> preturn
-    else
-      //Reply(Error, expectedError (sprintf "unknown scalar type: %s" w))
-//      expected "scalar type"
-      fun stream -> Reply(Error, expected "scalar type")
+    pWord >>= fun w ->
+        if isScalarType w then
+            toScalarType w |> preturn
+        else
+            //Reply(Error, expectedError (sprintf "unknown scalar type: %s" w))
+//            expected "scalar type"
+            fun stream -> Reply(Error, expected "scalar type")
 
 type FieldRule =
-  | Required
-  | Optional
-  | Repeated
+    | Required
+    | Optional
+    | Repeated
 
 let fieldRuleMap =
-  [
-    "required", Required;
-    "optional", Optional;
-    "repeated", Repeated;
-  ]
-  |> Map.ofList
+    [
+        "required", Required;
+        "optional", Optional;
+        "repeated", Repeated;
+    ]
+    |> Map.ofList
 
 let toFieldRule s = fieldRuleMap.[s]
 let isFieldRule s = fieldRuleMap.ContainsKey s
 
 let pFieldRule =
-  pWord >>= fun w ->
-    if isFieldRule w then
-      toFieldRule w |> preturn
-    else
-      fun stream -> Reply(Error, expected "field type")
+    pWord >>= fun w ->
+        if isFieldRule w then
+            toFieldRule w |> preturn
+        else
+            fun stream -> Reply(Error, expected "field type")
 
 let pMessageName = pstring "message" >>. spaces1 >>. pWord
 
 /// a line with a field
 let pField =
-  parse {
-    do! spaces;
-    let! ft = pFieldRule
-    do! spaces1
-    let! st = pScalarType
-    do! spaces1
-    let! name = pWord
-    do! spaces
-    do! skipChar '='
-    do! spaces
-    let! order = pint32
-    do! spaces
-    do! skipChar ';'
-    do! pEndOfLine
-    return ft, st, name, order
-  }
+    parse {
+        do! spaces;
+        let! ft = pFieldRule
+        do! spaces1
+        let! st = pScalarType
+        do! spaces1
+        let! name = pWord
+        do! spaces
+        do! skipChar '='
+        do! spaces
+        let! order = pint32
+        do! spaces
+        do! skipChar ';'
+        do! pEndOfLine
+        return ft, st, name, order
+    }
 
 let pMessage =
-  parse {
-    let! mn = pMessageName
-    do! spaces1
-    do! skipChar '{'
-    do! pEndOfLine
-    let! f = many1 pField
-    do! skipChar '}'
-    return mn, f
-  }
+    parse {
+        let! mn = pMessageName
+        do! spaces1
+        do! skipChar '{'
+        do! pEndOfLine
+        let! f = many1 pField
+        do! skipChar '}'
+        return mn, f
+    }
