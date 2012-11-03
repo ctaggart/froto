9f58b9d99560c4f184e990bc47dbd3e5da98a2a8
Cameron Taggart <cameron.taggart@gmail.com>
2012-11-03 14:14:05 -0700

    v0.0.0.3

--- ProtoParser/Version.fs
+++ ProtoParser/Version.fs
@@ -3,6 +3,6 @@ module Froto.Parser.Version
 
 open System.Reflection
 
-[<assembly: AssemblyVersion("0.0.0.2")>]
-[<assembly: AssemblyFileVersion("0.0.0.2")>]
+[<assembly: AssemblyVersion("0.0.0.3")>]
+[<assembly: AssemblyFileVersion("0.0.0.3")>]
 do()
