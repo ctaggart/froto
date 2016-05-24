0bda48f33d1185bb57d976ebfb62be9fc61a24fe
jhugard <jayhugard@hotmail.com>
2016-05-24 09:10:13 -0700

    Clean up how serializable Records are defined. Move record serialization code to RecordSerializer.fs and make generic.

--- Core/RecordSerializer.fs
+++ Core/RecordSerializer.fs
@@ -272,6 +272,70 @@ module RecordSerializer =
 //---- Record Serialization Functions
 //
 
+(* Serialize *)
+    let inline serialize (m:^msg when ^msg : (member Serializer : ZeroCopyBuffer -> ZeroCopyBuffer ) ) zcb =
+        let serialize m zcb = (^msg : (member Serializer : ZeroCopyBuffer -> ZeroCopyBuffer) (m, zcb) )
+        zcb
+        |> serialize m
+
+    let inline serializedLength (m:^msg when ^msg : (member Serializer : ZeroCopyBuffer -> ZeroCopyBuffer ) ) =
+        let serialize m zcb = (^msg : (member Serializer : ZeroCopyBuffer -> ZeroCopyBuffer) (m, zcb) )
+        let nwb = NullWriteBuffer()
+        nwb |> serialize m |> ignore
+        nwb.Length
+
+    let inline serializedLengthDelimitedLength m =
+        let len = serializedLength m
+        let lenlen = Utility.varIntLenNoDefault (uint64 len)
+        (uint32 lenlen) + len
+
+    let inline serializeLengthDelimited m zcb =
+        zcb
+        |> WireFormat.encodeVarint (uint64 (serializedLength m))
+        |> serialize m
+
+(* Serializer with unknowns *)
+    let inline emplace (src:ArraySegment<byte>) (dst:ArraySegment<byte>) =
+        Array.Copy(src.Array, src.Offset, dst.Array, dst.Offset, src.Count)
+
+    let internal serializeUnknown = function
+        | RawField.Varint (n,v) ->
+            WireFormat.encodeFieldVarint n v
+        | RawField.LengthDelimited (n,v) ->
+            WireFormat.encodeFieldLengthDelimited n (v.Count|>uint32) (emplace v)
+        | RawField.Fixed32 (n,v) ->
+            WireFormat.encodeFieldFixed32 n v
+        | RawField.Fixed64 (n,v) ->
+            WireFormat.encodeFieldFixed64 n v
+
+    let rec serializeUnknowns zcb = function
+        | [] -> zcb
+        | h::t ->
+            serializeUnknown h zcb
+            |>  flip serializeUnknowns t
+        
+    let inline serializeWithUnknowns m unks zcb =
+        zcb
+        |> serialize m
+        |> (flip serializeUnknowns unks)
+
+    let inline serializedLengthWithUnknowns (m:^msg when ^msg : (member Serializer : ZeroCopyBuffer -> ZeroCopyBuffer ) ) unks =
+        let serialize m zcb = (^msg : (member Serializer : ZeroCopyBuffer -> ZeroCopyBuffer) (m, zcb) )
+        let nwb = NullWriteBuffer()
+        nwb |> serializeWithUnknowns m unks |> ignore
+        nwb.Length
+
+    let inline serializedLengthDelimitedLengthWithUnknowns m unks =
+        let len = serializedLengthWithUnknowns m unks
+        let lenlen = Utility.varIntLenNoDefault (uint64 len)
+        (uint32 lenlen) + len
+
+    let inline serializeLengthDelimitedWithUnknowns m unks zcb =
+        zcb
+        |> WireFormat.encodeVarint (uint64 (serializedLengthWithUnknowns m unks))
+        |> serializeWithUnknowns m unks
+
+(* Deserialize *)
     let findDecoders decoderRing decodeUnknown (field:Encoding.RawField) =
         let n = field.FieldNum
         match decoderRing |> Map.tryFind n with
@@ -283,3 +347,51 @@ module RecordSerializer =
         |> Seq.map (findDecoders decoderRing decodeUnknown)
         |> Seq.fold (fun acc (fn, fld) -> fn acc fld) msgAcc
 
+
+    let inline deserializeFields (m:^msg when ^msg : (member DecoderRing : Map<int,^msg -> Encoding.RawField -> ^msg>)) fields =
+        let decoderRing = (^msg : (member DecoderRing: Map<int,^msg -> Encoding.RawField -> ^msg>) (m) )
+        let ignoreUnknown m fld = m
+        let result =
+            hydrate decoderRing ignoreUnknown m fields
+        result
+
+    let inline decodeFixup (m:^msg when ^msg : (static member DecodeFixup : ^msg -> ^msg)) =
+        let decodeFixup m = (^msg : (static member DecodeFixup : ^msg -> ^msg) (m) )
+        decodeFixup m
+
+    let inline deserializeLengthDelimited m zcb =
+        zcb
+        |> Utility.decodeLengthDelimited
+        |> deserializeFields m
+        |> decodeFixup
+
+    let inline deserialize m zcb =
+        zcb
+        |> Utility.decodeBuffer
+        |> deserializeFields m
+        |> decodeFixup
+
+
+    let inline deserializeFieldsWithUnknowns (m:^msg when ^msg : (member DecoderRing : Map<int,^msg -> Encoding.RawField -> ^msg>)) fields =
+        let decoderRing = (^msg : (member DecoderRing: Map<int,^msg -> Encoding.RawField -> ^msg>) (m) )
+        let unks = ref List.empty
+        let captureUnknown m fld =
+            unks := fld :: !unks
+            m
+        let result =
+            hydrate decoderRing captureUnknown m fields
+        (result, List.rev !unks)
+
+    let inline deserializeLengthDelimitedWithUnknowns m zcb =
+        let (m,unks) =
+            zcb
+            |> Utility.decodeLengthDelimited
+            |> deserializeFieldsWithUnknowns m
+        (decodeFixup m, unks)
+
+    let inline deserializeWithUnknowns m zcb =
+        let (m,unks) =
+            zcb
+            |> Utility.decodeBuffer
+            |> deserializeFieldsWithUnknowns m
+        (decodeFixup m, unks)
--- Froto.Core.Test/ExampleProtoRecord.fs
+++ Froto.Core.Test/ExampleProtoRecord.fs
@@ -1,11 +1,18 @@
 ﻿module SampleProtoRecord
 
-module InnerMessage =
+module SampleNamespace =
     open Froto.Core
     open Froto.Core.Encoding.Utility
     open Froto.Core.Encoding.RecordSerializer
+
+    type ETest =
+        | Nada = 0
+        | One = 1
+        | Two = 2
+
+    let ETestDefault = ETest.One  // NOTE: Non-zero default is only supported in Proto2
     
-    type InnerMessage = {
+    type InnerSample = {
         id : int32
         name : string
         option : bool
@@ -13,7 +20,8 @@ module InnerMessage =
         packedFixed32 : uint32 list
         repeatedInt32 : int32 list
         }
-        with static member Default = {
+        with
+            static member Default = {
                 id=0
                 name=""
                 option=false
@@ -21,80 +29,48 @@ module InnerMessage =
                 packedFixed32=[]
                 repeatedInt32=[]
             }
-    and ETest =
-        | Nada = 0
-        | One = 1
-        | Two = 2
-    
-    let ETestDefault = ETest.One  // NOTE: Non-zero default is only supported in Proto2
 
-    let serialize (m:InnerMessage) =
-        (m.id            |> dehydrateVarint 1) >>
-        (m.name          |> dehydrateString 2) >>
-        (m.option        |> dehydrateBool 3) >>
-        (m.test          |> dehydrateDefaultedVarint ETestDefault 4) >>
-        (m.packedFixed32 |> dehydratePackedFixed32 5) >>
-        (m.repeatedInt32 |> dehydrateRepeated dehydrateVarint 6)
+            member m.Serialize zcb                      = zcb |> serialize m
+            member m.Deserialize zcb                    = zcb |> deserialize m
+            member m.SerializeLengthDelimited zcb       = zcb |> serializeLengthDelimited m
+            member m.DeserializeLengthDelimited zcb     = zcb |> deserializeLengthDelimited m
 
-    let serializedLength (m:InnerMessage) =
-        let nwb = NullWriteBuffer()
-        nwb |> serialize m |> ignore
-        nwb.Length
+            member m.SerializeWithUnknowns unks zcb                 = zcb |> serializeWithUnknowns m unks
+            member m.DeserializeWithUnknowns zcb                    = zcb |> deserializeWithUnknowns m
+            member m.SerializeLengthDelimitedWithUnknowns unks zcb  = zcb |> serializeLengthDelimitedWithUnknowns m unks
+            member m.DeserializeLengthDelimitedWithUnknowns zcb     = zcb |> deserializeLengthDelimitedWithUnknowns m
+                
+            member m.Serializer zcb =
+                (m.id            |> dehydrateVarint 1) >>
+                (m.name          |> dehydrateString 2) >>
+                (m.option        |> dehydrateBool 3) >>
+                (m.test          |> dehydrateDefaultedVarint ETestDefault 4) >>
+                (m.packedFixed32 |> dehydratePackedFixed32 5) >>
+                (m.repeatedInt32 |> dehydrateRepeated dehydrateVarint 6)
+                <| zcb
 
-    let serializedLengthDelimitedLength (m:InnerMessage) =
-        let len = serializedLength m
-        let lenlen = varIntLenNoDefault (uint64 len)
-        (uint32 lenlen) + len
+            member m.DecoderRing =
+                [
+                    1, fun m rawField -> { m with id = rawField |> hydrateInt32 }
+                    2, fun m rawField -> { m with name = rawField |> hydrateString }
+                    3, fun m rawField -> { m with option = rawField |> hydrateBool }
+                    4, fun m rawField -> { m with test = rawField |> hydrateEnum }
+                    5, fun m rawField -> { m with packedFixed32 = rawField |> hydratePackedFixed32 }
+                    6, fun m rawField -> { m with repeatedInt32 = (rawField |> hydrateInt32) :: m.repeatedInt32 }
+                ]
+                |> Map.ofList
 
-    let serializeLengthDelimited (m:InnerMessage) zcb =
-        zcb
-        |> WireFormat.encodeVarint (uint64 (serializedLength m))
-        |> serialize m
-
-
-
-    let internal decoderRing =
-        [
-            1, fun m rawField -> { m with id = rawField |> hydrateInt32 }
-            2, fun m rawField -> { m with name = rawField |> hydrateString }
-            3, fun m rawField -> { m with option = rawField |> hydrateBool }
-            4, fun m rawField -> { m with test = rawField |> hydrateEnum }
-            5, fun m rawField -> { m with packedFixed32 = rawField |> hydratePackedFixed32 }
-            6, fun m rawField -> { m with repeatedInt32 = (rawField |> hydrateInt32) :: m.repeatedInt32 }
-        ]
-        |> Map.ofList
-
-    let internal decodeFixup (m:InnerMessage) =
-        { m with
-            packedFixed32 = List.rev m.packedFixed32
-            repeatedInt32 = List.rev m.repeatedInt32 }
-
-    let internal deserializeFields fields =
-//        let unk = ref List.empty
-//        let collectUnk m fld =
-//            unk := fld :: !unk
-//            m
-        let ignore m fld = m
-        let result =
-            hydrate decoderRing ignore InnerMessage.Default fields
-        result
-
-    let deserializeLengthDelimited zcb =
-        zcb
-        |> decodeLengthDelimited
-        |> deserializeFields
-
-    let deserializeBuffer zcb =
-        zcb
-        |> decodeBuffer
-        |> deserializeFields
-
+            static member DecodeFixup m =
+                { m with
+                    packedFixed32 = List.rev m.packedFixed32
+                    repeatedInt32 = List.rev m.repeatedInt32 }
 
 module PerformanceTest =
 
     open Xunit
     open FsUnit.Xunit
-    open InnerMessage
+    open Froto.Core.Encoding.RecordSerializer
+    open SampleNamespace
 
     [<Fact>]
     let ``Test Serialization and Deserialization Performance`` () =
@@ -126,10 +102,9 @@ module PerformanceTest =
             let zcr = Froto.Core.ZeroCopyBuffer(zcw.Array)
             seq {
                 while not zcr.IsEof do
-                    yield zcr |> deserializeLengthDelimited
+                    yield zcr |> deserializeLengthDelimited InnerSample.Default
             }
 
         ys |> Seq.iter ignore
 
         1 |> should equal 1
-
