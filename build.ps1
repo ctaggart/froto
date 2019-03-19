f06d55ad35191392a795697879ac8ddd0867069f
Cameron Taggart <cameron.taggart@gmail.com>
2019-03-19 13:29:06 -0300

    add version.json

--- build.ps1
+++ build.ps1
@@ -1,22 +1,10 @@
-$version = '0.7.0' # the version under development, update after a release
-$versionSuffix = '-build.0' # manually incremented for local builds
- 
-function isVersionTag($tag){
-    $v = New-Object Version
-    [Version]::TryParse($tag, [ref]$v)
-}
-
-if ($env:appveyor){
-    $versionSuffix = '-build.' + $env:appveyor_build_number
-    if ($env:appveyor_repo_tag -eq 'true' -and (isVersionTag($env:appveyor_repo_tag_name))){
-        $version = $env:appveyor_repo_tag_name
-        $versionSuffix = ''
-    }
-    Update-AppveyorBuild -Version "$version$versionSuffix"
-}
-
-dotnet build -c Release Froto.sln /p:Version=$version$versionSuffix
+dotnet build -c Release Froto.sln
+if ($lastexitcode -ne 0){ exit $lastexitcode }
 dotnet test --no-build -c Release Parser.Test\Froto.Parser.Test.fsproj
+if ($lastexitcode -ne 0){ exit $lastexitcode }
 dotnet test --no-build -c Release Serialization.Test\Froto.Serialization.Test.fsproj
-dotnet pack --no-build -c Release Parser\Froto.Parser.fsproj /p:Version=$version$versionSuffix -o $psscriptroot/bin
-dotnet pack --no-build -c Release Serialization\Froto.Serialization.fsproj /p:Version=$version$versionSuffix -o $psscriptroot/bin
+if ($lastexitcode -ne 0){ exit $lastexitcode }
+dotnet pack --no-build -c Release Parser\Froto.Parser.fsproj -o $psscriptroot/bin
+if ($lastexitcode -ne 0){ exit $lastexitcode }
+dotnet pack --no-build -c Release Serialization\Froto.Serialization.fsproj -o $psscriptroot/bin
+if ($lastexitcode -ne 0){ exit $lastexitcode }
