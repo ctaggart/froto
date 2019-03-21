8dfae8701980e06ce81e62e0a8e448e4885a3cbb
Cameron Taggart <cameron.taggart@gmail.com>
2019-03-21 20:05:24 -0300

    add build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+dotnet restore Froto.sln
+dotnet build Froto.sln -c Release
+dotnet test --no-build -c Release Parser.Test/Froto.Parser.Test.fsproj
+dotnet test --no-build -c Release Serialization.Test/Froto.Serialization.Test.fsproj
+dotnet pack --no-build -c Release Parser/Froto.Parser.fsproj -o bin
+dotnet pack --no-build -c Release Serialization/Froto.Serialization.fsproj -o bin
