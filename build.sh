6801a9423fddaa767c14a70e35044c46f65ceff5
Cameron Taggart <cameron.taggart@gmail.com>
2019-03-22 09:40:51 -0300

    && \

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ dotnet test --no-build -c Release Parser.Test/Froto.Parser.Test.fsproj
 dotnet test --no-build -c Release Serialization.Test/Froto.Serialization.Test.fsproj
 dotnet pack --no-build -c Release Parser/Froto.Parser.fsproj -o $SCRIPTROOT/bin
 dotnet pack --no-build -c Release Serialization/Froto.Serialization.fsproj -o $SCRIPTROOT/bin
-exit 1
+exit 5
