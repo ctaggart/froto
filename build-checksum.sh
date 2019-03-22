409adab376a3122516ad4d5f506270f5936959d5
Cameron Taggart <cameron.taggart@gmail.com>
2019-03-22 08:58:07 -0300

    test script error handling with exit 1

---
+++ build-checksum.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+sha256sum Parser/bin/Release/netstandard2.0/Froto.Parser.dll
+sha256sum Serialization/bin/Release/netstandard2.0/Froto.Serialization.dll
+# sha256sum Compiler/bin/Release/netcoreapp2.1/Froto.Compiler.dll
+# sha256sum bin/*.nupkg
--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ dotnet build Froto.sln -c Release
 dotnet test --no-build -c Release Parser.Test/Froto.Parser.Test.fsproj
 dotnet test --no-build -c Release Serialization.Test/Froto.Serialization.Test.fsproj
 dotnet pack --no-build -c Release Parser/Froto.Parser.fsproj -o $SCRIPTROOT/bin
-dotnet pack --no-build -c Release Serialization/Froto.Serialization.fsproj -o $SCRIPTROOT/
+dotnet pack --no-build -c Release Serialization/Froto.Serialization.fsproj -o $SCRIPTROOT/bin
+exit 1
