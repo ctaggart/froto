ba983b63079bcf7949ede5641c437eb48df8b6e1
Cameron Taggart <cameron.taggart@gmail.com>
2019-03-22 14:43:04 -0300

    Set up CI with Azure Pipelines (#103)
    * Set up CI with Azure Pipelines
    
    * add build.sh
    
    * +x
    
    * add sha256sum bin/*.nupkg to appveyor
    
    * use full path for -o & pipelines copying files
    
    * indent script more
    
    * flattenFolders: true
    
    * <Deterministic>true</Deterministic>
    
    * <OtherFlags>--deterministic+</OtherFlags>
    
    * add artifacts
    
    * try artifactType: filePath
    
    * what is targetPath
    
    * itemPath is nuget
    
    * test script error handling with exit 1
    
    * chmod +x
    
    * &&
    
    * && same line
    
    * && \
    
    * set -e
    
    * set -e works, remove exit
    
    * add nuget lock files
    
    * locking may need some work
    
    * remove nupkg checksums

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+SCRIPTROOT="$(cd "$(dirname "$0")"; pwd)"
+dotnet restore Froto.sln
+dotnet build Froto.sln -c Release
+dotnet test --no-build -c Release Parser.Test/Froto.Parser.Test.fsproj
+dotnet test --no-build -c Release Serialization.Test/Froto.Serialization.Test.fsproj
+dotnet pack --no-build -c Release Parser/Froto.Parser.fsproj -o $SCRIPTROOT/bin
+dotnet pack --no-build -c Release Serialization/Froto.Serialization.fsproj -o $SCRIPTROOT/bin
