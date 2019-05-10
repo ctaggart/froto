6ed1d1b750714b8fe2e69ec7476e8c576aeb1ecd
takemyoxygen <uladzimir.makarau@gmail.com>
2019-05-10 21:48:23 +0300

    Remove TP tests project from sln; make paths in TP test script relative

--- TypeProvider.Tests/test.fsx
+++ TypeProvider.Tests/test.fsx
@@ -1,11 +1,12 @@
-#r @"D:\dev\code\open-source\froto\TypeProvider.DesignTime\bin\Debug\netstandard2.0\FParsec.dll"
-#r @"D:\dev\code\open-source\froto\TypeProvider.DesignTime\bin\Debug\netstandard2.0\FParsecCS.dll"
-#r @"D:\dev\code\open-source\froto\TypeProvider.DesignTime\bin\Debug\netstandard2.0\Froto.Parser.dll"
-#r @"D:\dev\code\open-source\froto\TypeProvider.DesignTime\bin\Debug\netstandard2.0\Froto.Serialization.dll"
-#r @"D:\dev\code\open-source\froto\TypeProvider.Runtime\bin\Debug\netstandard2.0\Froto.TypeProvider.DesignTime.dll"
-#r @"D:\dev\code\open-source\froto\TypeProvider.Runtime\bin\Debug\netstandard2.0\Froto.TypeProvider.Runtime.dll"
+#r @"..\TypeProvider.DesignTime\bin\Debug\netstandard2.0\FParsec.dll"
+#r @"..\TypeProvider.DesignTime\bin\Debug\netstandard2.0\FParsecCS.dll"
+#r @"..\TypeProvider.DesignTime\bin\Debug\netstandard2.0\Froto.Parser.dll"
+#r @"..\TypeProvider.DesignTime\bin\Debug\netstandard2.0\Froto.Serialization.dll"
+#r @"..\TypeProvider.DesignTime\bin\Debug\netstandard2.0\Froto.TypeProvider.DesignTime.dll"
 
 
 open Froto.TypeProvider
 
-type Ololo = ProtocolBuffersTypeProvider<"D:/dev/code/open-source/froto/TypeProvider.Tests/Proto/type_provider_test.proto">
+[<Literal>]
+let protoFile = __SOURCE_DIRECTORY__ + "/Proto/type_provider_test.proto"
+type Ololo = ProtocolBuffersTypeProvider<protoFile>
