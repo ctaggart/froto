05e618dd97f147b34cf0fb94846758f9b7ad0034
Cameron Taggart <cameron.taggart@gmail.com>
2019-08-17 04:43:01 +0800

    GitHub Action for Build (#108)
    * basic github action template
    
    * docker --version
    
    * ./build.sh
    
    * set up 2.2.105
    
    * try 3.0.0-preview8
    
    * 3.0.100-preview8
    
    * try 3.0.100-preview8-013656
    
    * variables from step to step?
    
    * run
    
    * echo ?
    
    * add version
    
    * tab
    
    * dll load errors fixed in dotnet 3
    
    * update deps
    
    * deps in TypeProvider too
    
    * build, test, and pack
    
    * deps again
    
    * test nbgv
    
    * $HOME
    
    * push
    
    * *.nupkg
    
    * nbgv for design time
    
    * lock

--- build.ps1
+++ build.ps1
@@ -1,23 +1,25 @@
+# build, test, pack
+
+# dotnet restore Froto.sln --force-evaluate
+# dotnet restore TypeProvider.Test\Froto.TypeProvider.Test.fsproj --force-evaluate
+
 dotnet build -c Release Froto.sln
+if ($lastexitcode -ne 0){ exit $lastexitcode }
+dotnet build -c Release TypeProvider.Test\Froto.TypeProvider.Test.fsproj
 if ($lastexitcode -ne 0){ exit $lastexitcode }
 
 dotnet test --no-build -c Release Parser.Test\Froto.Parser.Test.fsproj
 if ($lastexitcode -ne 0){ exit $lastexitcode }
-
 dotnet test --no-build -c Release Serialization.Test\Froto.Serialization.Test.fsproj
 if ($lastexitcode -ne 0){ exit $lastexitcode }
-
-dotnet build -c Release TypeProvider.Test\Froto.TypeProvider.Test.fsproj
-if ($lastexitcode -ne 0){ exit $lastexitcode }
-
 dotnet test --no-build -c Release TypeProvider.Test\Froto.TypeProvider.Test.fsproj
 if ($lastexitcode -ne 0){ exit $lastexitcode }
 
 dotnet pack --no-build -c Release Parser\Froto.Parser.fsproj -o $psscriptroot/bin
 if ($lastexitcode -ne 0){ exit $lastexitcode }
-
 dotnet pack --no-build -c Release Serialization\Froto.Serialization.fsproj -o $psscriptroot/bin
 if ($lastexitcode -ne 0){ exit $lastexitcode }
-
+dotnet pack --no-build -c Release TypeProvider.DesignTime\Froto.TypeProvider.DesignTime.fsproj -o $psscriptroot/bin
+if ($lastexitcode -ne 0){ exit $lastexitcode }
 dotnet pack --no-build -c Release TypeProvider.Runtime\Froto.TypeProvider.Runtime.fsproj -o $psscriptroot/bin
 if ($lastexitcode -ne 0){ exit $lastexitcode }
