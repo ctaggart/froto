810d7d6c8ee60fd4810802fef316dd64cf2a5cad
Cameron Taggart <cameron.taggart@gmail.com>
2019-08-17 02:54:29 +0800

    deps in TypeProvider too

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/sh -e
 SCRIPTROOT="$(cd "$(dirname "$0")"; pwd)"
-VERSION="${VERSION:-0.0.0}"
 dotnet restore Froto.sln
-dotnet build Froto.sln -c Release /p:Version=$VERSION
+dotnet build Froto.sln -c Release
 dotnet test --no-build -c Release Parser.Test/Froto.Parser.Test.fsproj 
 dotnet test --no-build -c Release Serialization.Test/Froto.Serialization.Test.fsproj
 dotnet test --no-build -c Release TypeProvider.Test/Froto.TypeProvider.Test.fsproj
-dotnet pack --no-build -c Release Parser/Froto.Parser.fsproj -o $SCRIPTROOT/bin /p:Version=$VERSION
-dotnet pack --no-build -c Release Serialization/Froto.Serialization.fsproj -o $SCRIPTROOT/bin /p:Version=$VERSION
-dotnet pack --no-build -c Release TypeProvider.Test/Froto.TypeProvider.Test.fsproj $SCRIPTROOT/bin /p:Version=$VERSION
+dotnet pack --no-build -c Release Parser/Froto.Parser.fsproj -o $SCRIPTROOT/bin
+dotnet pack --no-build -c Release Serialization/Froto.Serialization.fsproj -o $SCRIPTROOT/bin
+dotnet pack --no-build -c Release TypeProvider.Test/Froto.TypeProvider.Test.fsproj $SCRIPTROOT/bin
 dotnet build Froto.sln -c Release
 
 dotnet test --no-build -c Release Parser.Test/Froto.Parser.Test.fsproj
